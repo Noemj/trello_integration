651f46a17a42340531ac8dd456ab3305ccd87387
Olli Rissanen <olli.rissanen@helsinki.fi>
2014-05-18 00:00:16 +0300

    Changed GitHub url to html page url

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/issue', function(req, resp){
     var options = {
       url: 'https://api.trello.com/1/lists/' + list + '/cards?key=' + key + '&token=' + token + '',
       method: 'POST',
-      body: '{"name": "'+ req.body.issue.title + '", "desc": "' + req.body.issue.url + '\\n' + req.body.issue.body + '"}',
+      body: '{"name": "'+ req.body.issue.title + '", "desc": "' + req.body.issue.html_url + '\\n' + req.body.issue.body + '"}',
       headers: {
         'Content-Type': 'application/json'
       }
